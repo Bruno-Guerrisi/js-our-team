b7a3263b02f9ddd160ee0fe851ba6be58bd0f65d
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-04 14:40:21 +0100

    setup

---
+++ main.js
@@ -0,0 +1,11 @@
+// Creiamo il nostro array di oggetti che rappresentano ciascun membro del team
+
+const users = [
+
+    {
+        name: '',
+        role: '',
+        imageUser: ''
+    },
+
+];
